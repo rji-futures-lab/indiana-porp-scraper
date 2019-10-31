0272c90f3b16e2d54addb6081fb9b1b914710662
Kelly <kellykenoyerwriter@gmail.com>
2019-10-31 16:25:07 -0500

    more changes

--- page-scrape.py
+++ page-scrape.py
@@ -3,7 +3,13 @@ from bs4 import BeautifulSoup
 from models import RestrainingOrder
 from time import sleep
 
-query = RestrainingOrder.select().where(RestrainingOrder.date_filed.endswith('2017') | RestrainingOrder.date_filed.endswith('2018') | RestrainingOrder.date_filed.endswith('2019'))
+query = RestrainingOrder.select().where(
+	(
+		RestrainingOrder.date_filed.endswith('2017') | 
+		RestrainingOrder.date_filed.endswith('2018') | 
+		RestrainingOrder.date_filed.endswith('2019')
+	) & RestrainingOrder.case_number.contains('PO')
+)
 	# url = f'https://mycourts.in.gov/PORP/Search/Detail?ID={ro.id}'
 	# r = requests.get(url)
 	# html = r.content
