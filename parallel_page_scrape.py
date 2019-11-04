5979cc10318dc0fa674af2d06b19a19599ea190d
James Gordon <gordon.je@gmail.com>
2019-11-03 19:04:26 -0600

    add parallel scraping script

---
+++ parallel_page_scrape.py
@@ -0,0 +1,40 @@
+import argparse
+import multiprocessing
+import os
+from time import sleep
+from recent_protection_orders import query
+from page_scrape import make_requests
+
+
+def get_chunks(num_chunks):
+    total = query.count()
+    chunk_size = round(total / num_chunks)
+    # For item i in a range that is the count of the query,
+    for i in range(0, total, chunk_size):
+        # Create an index range for l of n items:
+        yield query[i:i+chunk_size]
+
+
+def handle_chunk(ids):
+    sleep(30)
+    make_requests(ids)
+    
+
+def download_all_chunks(chunks):
+    num_pools = len(chunks)
+    with multiprocessing.Pool(num_pools) as pool:
+        pool.map(handle_chunk, chunks)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num_processes', type=int)
+    args = parser.parse_args()
+    
+    os.makedirs('.cache/SearchDetail/', exist_ok=True)
+    
+    chunks = list(
+        get_chunks(args.num_processes)
+    )
+
+    download_all_chunks(chunks)
