2fc856d745ce69d1120726e582638a62cac016ac
Kelly <kellykenoyerwriter@gmail.com>
2019-11-06 13:01:08 -0600

    attempted to add city

--- extract_details.py
+++ extract_details.py
@@ -1,14 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 from models import RestrainingOrder
+import os, re
+
+files = [
+	f for f in os.listdir(".cache/SearchDetail")
+	if '.html' in f
+	]
+
+
+for f in files:
+	ro_id = int(f[:-5])
+
+	ro = RestrainingOrder.get(RestrainingOrder.id == ro_id)
+
+
+	with open(f".cache/SearchDetail/{f}") as file:
+
+
+		html = file.read()
 
 	soup = BeautifulSoup(html, "html.parser")
 
 	orders = soup.find('ul', {'class': 'orders'})
+	# details = soup.find('div', {'id': 'form-con'})
+
+	# city = details.find(ur'City:(.*)' , re.DOTALL)
+	# print(city)
+	# break
 
 
 	lis = orders.find_all('li')
 	library = {}
+
+	
+
 	for li in lis:
 		p_tags = li.find_all('p')
 		label = p_tags[0].text.strip()
@@ -17,11 +43,6 @@ from models import RestrainingOrder
 		except IndexError:
 			ro.not_served=True
 		library[label] = data
-
-	print(library)
-
-
-
 
 	ro.date_issued = library['Order issued on:']
 	try:
@@ -40,6 +61,7 @@ from models import RestrainingOrder
 		ro.dismissal_reason = library['Dismissal Reason']
 	except KeyError:
 		pass
+
 	ro.save()
 
-	sleep(2)
+
